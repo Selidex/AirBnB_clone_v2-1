ba0ae2545fd3b54adff6f90a72afb6a047c6df21
brerickner <breannarickner@gmail.com>
2021-05-07 19:23:07 +0000

    adding progress for task 5. Needed to adjust code to be plural things

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Create an endpoint that retrieves the number of each objects by type"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -10,10 +10,6 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from models import storage
-
-
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status')
@@ -26,6 +22,10 @@ def status():
 def stats():
     """Returns Status:OK"""
     obj_dict = {}
-    for obj in classes:
-        obj_dict[obj] = storage.count(classes[obj])
+    obj_dict['amenities'] = storage.count(Amenity)
+    obj_dict['states'] = storage.count(State)
+    obj_dict['cities'] = storage.count(City)
+    obj_dict['places'] = storage.count(Place)
+    obj_dict['reviews'] = storage.count(Review)
+    obj_dict['users'] = storage.count(User)
     return jsonify(obj_dict)
