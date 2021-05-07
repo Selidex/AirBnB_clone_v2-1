6180f1634d1745a90180608453c3a5da4ea6777b
Selidex <justindparnell@yahoo.com>
2021-05-07 18:04:10 +0000

    Uploading first working flask

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Still no clue what I am doing here"""
 from api.v1.views import app_views
-import jsonify
+from flask import jsonify
 
 @app_views.route('/status')
 def status():
     """Returns Status:OK"""
-    return jsonify({'Status' : 'OK'})
+    return jsonify({'status' : 'OK'})
