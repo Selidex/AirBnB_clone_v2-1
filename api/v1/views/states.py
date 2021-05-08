7b80052a44b016127cc7afac7499346e5451f4b0
brerickner <breannarickner@gmail.com>
2021-05-08 20:06:34 +0000

    adding progress for task 7. pepfixed output good for get and delete

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """Create a new view for State objects that handles all
 default RestFul API actions"""
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 from models import storage
 from models.state import State
 
@@ -13,13 +13,25 @@ def states():
     obj_list = []
     for state in storage.all('State').values():
         obj_list.append(state.to_dict())
+    print(obj_list)
     return jsonify(obj_list)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def states_id(state_id):
     """Retrieves the list of all State objects"""
-    obj_dict = storage.get(State, state_id)
-    if obj_dict is None:
+    obj = storage.get(State, state_id)
+    if obj is None:
         abort(404)
-    return jsonify(obj_dict.to_dict())
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def del_states_id(state_id):
+    """Retrieves the list of all State objects"""
+    obj = storage.get(State, state_id)
+    if obj is None:
+        abort(404)
+    obj.delete()
+    storage.save()
+    return jsonify({}), 200
