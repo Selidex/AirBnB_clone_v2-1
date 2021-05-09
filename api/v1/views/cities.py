03394d74a4351fa7442fea70dd44e3a56675df4f
Selidex <justindparnell@yahoo.com>
2021-05-09 07:59:28 +0000

    Comment fix in cities, first upload amenities, updating imports __init__

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Create a new view for State objects that handles all
+"""Create a new view for City objects that handles all
 default RestFul API actions"""
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
